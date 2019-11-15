076a6924122b6d5c3739bcd73be317425b9fe4ff
gam0022 <gam0022@gmail.com>
2019-11-15 10:24:23 +0900

    npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  // argv.mode === 'production' で切り替えたい
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    inline: true,
+    hot: true,
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',

c3019ce93cf4aaa9e6e7bf4fb42d4682b3cba548
gam0022 <gam0022@gmail.com>
2019-11-17 22:53:06 +0900

    npm install --save-dev typescript ts-loader

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),

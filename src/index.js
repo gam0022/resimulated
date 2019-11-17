008218962abc4f7551a2d3ed1130b652d4f7e9c0
gam0022 <gam0022@gmail.com>
2019-11-17 20:29:45 +0900

    Delete uniform buffer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ window.addEventListener("load", ev => {
     canvas.width = 512, canvas.height = 512;
     canvas.style.border = "solid";
     document.body.appendChild(canvas);
+
     // webgl2 enabled default from: firefox-51, chrome-56
     const gl = canvas.getContext("webgl2");
     gl.enable(gl.CULL_FACE);
@@ -47,20 +48,6 @@ window.addEventListener("load", ev => {
             value: [0, 0, 0, 0],
         },
     };
-
-    // opengl3 uniform buffer
-    // NOTE: each data attribute required 16 byte
-    const screenData = new Float32Array([canvas.width, canvas.height, 0, 0]);
-    const screenBuf = gl.createBuffer();
-    gl.bindBuffer(gl.UNIFORM_BUFFER, screenBuf);
-    gl.bufferData(gl.UNIFORM_BUFFER, screenData, gl.DYNAMIC_DRAW);
-    gl.bindBuffer(gl.UNIFORM_BUFFER, null);
-
-    const timerData = new Uint32Array([0, 0, 0, 0]);
-    const timerBuf = gl.createBuffer();
-    gl.bindBuffer(gl.UNIFORM_BUFFER, timerBuf);
-    gl.bufferData(gl.UNIFORM_BUFFER, timerData, gl.DYNAMIC_DRAW);
-    gl.bindBuffer(gl.UNIFORM_BUFFER, null);
     
     // opengl3 VAO
     const vertexArray = gl.createVertexArray();
@@ -76,8 +63,7 @@ window.addEventListener("load", ev => {
         const elem = gl.FLOAT, count = vert2d[0].length, normalize = false;
         const offset = 0, stride = count * Float32Array.BYTES_PER_ELEMENT;
         gl.enableVertexAttribArray(vert2dId);
-        gl.vertexAttribPointer(
-            vert2dId, count, elem, normalize, stride, offset);
+        gl.vertexAttribPointer(vert2dId, count, elem, normalize, stride, offset);
         gl.bindVertexArray(null);
         //NOTE: these unbound buffers is not required; works fine if unbound
         //gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -125,34 +111,12 @@ window.addEventListener("load", ev => {
         return program;
     };
 
-    const render = (program, count, timestamp) => {
-        // set timer variable to update the uniform buffer
-        timerData[0] = count;
-        gl.bindBuffer(gl.UNIFORM_BUFFER, timerBuf);
-        gl.bufferData(gl.UNIFORM_BUFFER, timerData, gl.DYNAMIC_DRAW);
-        gl.bindBuffer(gl.UNIFORM_BUFFER, null);
-
-        // uniform buffer binding
-        /*let uniformIndex = 0;
-        const screenId = gl.getUniformBlockIndex(program, "Screen");
-        //console.log("screen uniform size", gl.getActiveUniformBlockParameter(
-        //    program, screenId, gl.UNIFORM_BLOCK_DATA_SIZE)); //=> 4x4=16
-        gl.uniformBlockBinding(program, screenId, ++uniformIndex);
-        gl.bindBufferBase(gl.UNIFORM_BUFFER, uniformIndex, screenBuf);
-
-        const timerId = gl.getUniformBlockIndex(program, "Timer");
-        //console.log("timer uniform size", gl.getActiveUniformBlockParameter(
-        //    program, timerId, gl.UNIFORM_BLOCK_DATA_SIZE)); //=> 4x4=16
-        gl.uniformBlockBinding(program, timerId, ++uniformIndex);
-        gl.bindBufferBase(gl.UNIFORM_BUFFER, uniformIndex, timerBuf);*/        
-        
+    const render = (program, timestamp, lastTimestamp) => {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.useProgram(program);
-        // draw the buffer with VAO
-        // NOTE: binding vert and index buffer is not required
 
         uniforms.iTime.value = timestamp * 0.001;
-        // uniforms.iTimeDelta.value = delta;
+        uniforms.iTimeDelta.value = (timestamp - lastTimestamp) * 0.001;
         uniforms.iFrame.value++;
 
         Object.keys(uniforms).forEach((key) => {
@@ -161,10 +125,11 @@ window.addEventListener("load", ev => {
             gl[`uniform${method}`](uniforms[key].location, uniforms[key].value);
         });
 
+        // draw the buffer with VAO
+        // NOTE: binding vert and index buffer is not required
         gl.bindVertexArray(vertexArray);
         const indexOffset = 0 * index[0].length;
-        gl.drawElements(gl.TRIANGLES, indexData.length,
-                        gl.UNSIGNED_SHORT, indexOffset);
+        gl.drawElements(gl.TRIANGLES, indexData.length, gl.UNSIGNED_SHORT, indexOffset);
         const error = gl.getError();
         if (error !== gl.NO_ERROR) console.log(error);
         gl.bindVertexArray(null);
@@ -172,10 +137,11 @@ window.addEventListener("load", ev => {
     };
 
     const startRendering = (program) => {
-        let count = 0;
+        let lastTimestamp = 0;
         const animate = (timestamp) => {
-            render(program, count++, timestamp);
+            render(program, timestamp, lastTimestamp);
             requestAnimationFrame(animate);
+            lastTimestamp = timestamp;
         };
         animate();
     };
@@ -184,8 +150,6 @@ window.addEventListener("load", ev => {
     const cleanupResources = (program) => {
         gl.deleteBuffer(vertBuf);
         gl.deleteBuffer(indexBuf);
-        gl.deleteBuffer(screenBuf);
-        gl.deleteBuffer(timerBuf);
         gl.deleteVertexArray(vertexArray);
         gl.deleteProgram(program);
     };

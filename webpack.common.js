2d751b1fba495c8301b66c47ad244cfa4123e212
gam0022 <gam0022@gmail.com>
2019-11-17 13:50:58 +0900

    rename output filename to bundle.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
 const path = require('path');
-//const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   /*entry: {

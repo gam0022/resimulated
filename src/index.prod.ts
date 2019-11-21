9dc7bbfdb003a9640981b71f6e7f96a839b5c36c
gam0022 <gam0022@gmail.com>
2019-11-21 10:24:44 +0900

    dist/style.prod.min.css

--- src/index.prod.ts
+++ src/index.prod.ts
@@ -4,6 +4,6 @@ window.addEventListener("load", ev => {
     const player = new ShaderPlayer();
 
     const style = document.createElement("style");
-    style.innerText = require("./style.prod.css").default;
+    style.innerText = require("../dist/style.prod.min.css").default;
     document.head.appendChild(style);
 }, false);

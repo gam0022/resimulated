3753278b46abf50aef1721bacd661295769344e9
gam0022 <gam0022@gmail.com>
2019-11-24 23:28:03 +0900

    Use webpack.DefinePlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   entry: './src/index.dev.ts',
@@ -10,4 +11,9 @@ module.exports = merge(common, {
     hot: true,
     contentBase: './dist',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(false),
+    })
+  ],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   entry: './src/index.prod.ts',
   mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+    })
+  ],
 });

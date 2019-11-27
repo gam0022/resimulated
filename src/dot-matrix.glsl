02de9fd22a17f386458fd5dc0cb12e60d5b8bce1
gam0022 <gam0022@gmail.com>
2019-11-27 22:59:49 +0900

    Delete tab

--- src/dot-matrix.glsl
+++ src/dot-matrix.glsl
@@ -11,7 +11,7 @@ uniform sampler2D iPrevPass;
 #define saturate(x) clamp(x, 0.0, 1.0)
 
 float sdCircle(vec2 p, float r) {
-	return length(p) - r;
+    return length(p) - r;
 }
 
 // Dot Matrix
@@ -25,7 +25,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 
     vec3 col;
     vec2 uvDot = (((floor(uv * num) + 0.5) / num) + 1.0) * 0.5;
-   	vec3 lum = texture(iPrevPass, uvDot).rgb;
+       vec3 lum = texture(iPrevPass, uvDot).rgb;
 
     vec2 uvGrid = fract(uv * num);
     vec2 pGrid = uvGrid - 0.5;

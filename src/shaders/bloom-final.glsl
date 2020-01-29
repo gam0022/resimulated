20860785825ccfda8e241f8742e417ee00a16bc5
gam0022 <gam0022@gmail.com>
2020-01-29 15:29:04 +0900

    wip

--- src/shaders/bloom-final.glsl
+++ src/shaders/bloom-final.glsl
@@ -23,9 +23,9 @@ vec3 tap4(sampler2D tex, vec2 uv, vec2 texelSize)
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 	vec2 uv = fragCoord.xy / iResolution.xy;
-    vec2 texelSize = 1.0 / (2.0 * iResolution.xy);
+    vec2 texelSize = 1.0 / iResolution.xy * 0.5;
     vec3 col = texture(iPass0, uv).rgb;
-    fragColor = vec4(col + 3.0 * tap4(iPrevPass, uv, texelSize), 1.0);
+    fragColor = vec4(col + 10.0 * abs(sin(iTime)) * tap4(iPrevPass, uv, texelSize), 1.0);
 }
 
 out vec4 outColor;

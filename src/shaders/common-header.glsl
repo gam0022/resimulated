d30d6a7952e76b5ec24992f61f58fa54235593f8
gam0022 <gam0022@gmail.com>
2020-02-03 13:34:01 +0900

    7880 byte

--- src/shaders/common-header.glsl
+++ src/shaders/common-header.glsl
@@ -1,5 +1,7 @@
 #version 300 es
-precision highp float;precision highp int;precision mediump sampler3D;void mainImage(out vec4 a, in vec2 b);out vec4 outColor;void main(void){vec4 c;mainImage(c, gl_FragCoord.xy);outColor = c;}
+precision highp float;precision highp int;precision mediump sampler3D;
+void mainImage(out vec4 fragColor, in vec2 fragCoord);
+out vec4 outColor;void main(void){vec4 c;mainImage(c, gl_FragCoord.xy);outColor = c;}
 #define saturate(x) clamp(x, 0.0, 1.0)
 
 uniform vec3 iResolution;

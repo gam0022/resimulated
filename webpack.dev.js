771360ec364e67c34f39c48565b8495d39a397a9
gam0022 <gam0022@gmail.com>
2020-03-14 16:41:20 +0900

    PLAY_SOUND_FILE: false

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,6 +14,7 @@ module.exports = merge(common, {
     plugins: [new webpack.DefinePlugin({
         PRODUCTION: JSON.stringify(false),
         GLOBAL_UNIFORMS: JSON.stringify(true),
-        PLAY_SOUND_FILE: JSON.stringify('wormhole.mp3'),
+        // PLAY_SOUND_FILE: JSON.stringify('wormhole.mp3'),
+        PLAY_SOUND_FILE: JSON.stringify(false),
     })],
 });

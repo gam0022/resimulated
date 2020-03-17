55024d7633bea37944de146ad1b32c1a8f39a151
gam0022 <gam0022@gmail.com>
2020-03-17 17:18:37 +0900

    yoshi / 9295 byte

--- src/shaders/bloom-upsample.glsl
+++ src/shaders/bloom-upsample.glsl
@@ -1,12 +1,11 @@
 uniform sampler2D iBeforeBloom;
 uniform sampler2D iPairBloomDown;
 
-uniform float gBloomSpread;         // 1 1 2
-uniform float gBloomUpsampleScale;  // 0.25 0 1
+uniform float gBloomSpread;  // 1.3 1 2
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = fragCoord.xy / iResolution.xy;
-    vec2 texelSize = 1.0 / iResolution.xy * gBloomUpsampleScale;
+    vec2 texelSize = 1.0 / iResolution.xy;
     vec3 col = texture(iPairBloomDown, uv).rgb;
     fragColor = vec4(col + gBloomSpread * tap4(iPrevPass, uv, texelSize), 1.0);
 }

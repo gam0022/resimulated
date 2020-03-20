9beab53e2fe57760dddf749c17c66493e008e3e0
gam0022 <gam0022@gmail.com>
2020-03-20 15:35:55 +0900

    arp1()

--- src/shaders/sound-template.glsl
+++ src/shaders/sound-template.glsl
@@ -86,64 +86,7 @@ vec2 arp(float note, float time) {
     return amp * vec2(sine(freq * 0.99 * time + fm), sine(freq * 1.01 * time + fm));
 }
 
-// ------
-// main
-
-vec2 mainSound(float time) {
-    float beat = timeToBeat(time);
-    vec2 ret = vec2(0.0);
-
-    // ---
-    // kick
-
-    float kickTime = beatToTime(mod(beat, 1.0));
-    ret += 0.8 * kick(kickTime);
-
-    float sidechain = smoothstep(0.0, 0.4, kickTime);
-
-    // ---
-    // hihat
-
-    float hihatTime = beatToTime(mod(beat + 0.5, 1.0));
-    ret += 0.5 * hihat(hihatTime);
-
-    // ---
-    // bass
-
-    float bassNote = chord(0.0) - 24.0;
-    ret += sidechain * 0.6 * bass(bassNote, time);
-
-    // ---
-    // chord
-
-    ret += sidechain * 0.6 * vec2(pad(chord(0.0), time) + pad(chord(1.0), time) + pad(chord(2.0), time) + pad(chord(3.0), time)) / 4.0;
-
-    // ---
-    // arp
-
-    /*float arpTime = beatToTime(mod(beat, 0.25));
-
-    // ノート番号を指定していします
-    float[8 * 2] arpNotes = float[](
-        // 展開1
-        69.0, 70.0, 71.0, 72.0, 69.0, 70.0, 69.0, 72.0,
-
-        // 展開2
-        50.0, 51.0, 52.0, 53.0, 50.0, 51.0, 52.0, 53.0);
-
-    // 展開 0 -> 1 -> 0
-    int[3] offsets = int[](0, 1, 0);
-
-    // ノート番号を決定します
-    float arpNote = arpNotes[offsets[int(mod(beat / 8.0, 3.0))] * 8 + int(mod(beat, 8.0))];
-
-    ret += sidechain * 0.5 * vec2(arp(arpNote, arpTime));*/
-
-    //
-    // ARP1
-    //
-
-// ARP1のビート数
+vec2 arp1(float beat, float time) {
 #define ARP1_NUM_BEAT 8
 #define NOTE_DIV 4
 
@@ -228,9 +171,65 @@ vec2 mainSound(float time) {
     int index = int(indexFloat);
     float arp1Note = arp1Notes[index];
     float arp1Time = beatToTime((indexFloat - arp1Indexes[index]) / arp1Divs[index] * float(NOTE_DIV));
-    ret += 0.5 * vec2(arp(arp1Note, arp1Time));
+    return vec2(arp(arp1Note, arp1Time));
+}
+
+// ------
+// main
+
+vec2 mainSound(float time) {
+    float beat = timeToBeat(time);
+    vec2 ret = vec2(0.0);
 
     // ---
+    // kick
+
+    float kickTime = beatToTime(mod(beat, 1.0));
+    ret += 0.8 * kick(kickTime);
+
+    float sidechain = smoothstep(0.0, 0.4, kickTime);
+
+    // ---
+    // hihat
+
+    float hihatTime = beatToTime(mod(beat + 0.5, 1.0));
+    ret += 0.5 * hihat(hihatTime);
+
+    // ---
+    // bass
+
+    float bassNote = chord(0.0) - 24.0;
+    ret += sidechain * 0.6 * bass(bassNote, time);
+
+    // ---
+    // chord
+
+    ret += sidechain * 0.6 * vec2(pad(chord(0.0), time) + pad(chord(1.0), time) + pad(chord(2.0), time) + pad(chord(3.0), time)) / 4.0;
+
+    // ---
+    // arp
+
+    /*float arpTime = beatToTime(mod(beat, 0.25));
+
+    // ノート番号を指定していします
+    float[8 * 2] arpNotes = float[](
+        // 展開1
+        69.0, 70.0, 71.0, 72.0, 69.0, 70.0, 69.0, 72.0,
+
+        // 展開2
+        50.0, 51.0, 52.0, 53.0, 50.0, 51.0, 52.0, 53.0);
+
+    // 展開 0 -> 1 -> 0
+    int[3] offsets = int[](0, 1, 0);
+
+    // ノート番号を決定します
+    float arpNote = arpNotes[offsets[int(mod(beat / 8.0, 3.0))] * 8 + int(mod(beat, 8.0))];
+
+    ret += sidechain * 0.5 * vec2(arp(arpNote, arpTime));*/
+
+    // ---
+
+    ret += arp1(beat, time);
 
     return clamp(ret, -1.0, 1.0);
 }

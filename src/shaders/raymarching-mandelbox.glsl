e9495b7fee919f012d428a5be8fe7fad74c96d60
gam0022 <gam0022@gmail.com>
2020-03-25 18:19:45 +0900

    gLogoIntensity

--- src/shaders/raymarching-mandelbox.glsl
+++ src/shaders/raymarching-mandelbox.glsl
@@ -187,15 +187,16 @@ float calcEdge(vec3 p) {
     return edge;
 }
 
-uniform vec3 gEmissiveColor;   // 48 255 48
-uniform float gEmissiveSpeed;  // 1 0 2
-
 // Thanks https://shadertoy.com/view/ttsGR4
 float revisionLogo(vec2 p, float rot) {
     int[] pat = int[](0, ~0, 0x7C, 0xC0F03C00, 0xF7FBFF01, ~0, 0, 0x8320D39F, ~0, 0x1F0010, 0);
     int r = clamp(int(20. * length(p)), 0, 10);
     return float(pat[r] >> int(5.1 * atan(p.y, p.x) + 16. + (hash11(float(r * 1231)) - 0.5) * rot) & 1);
 }
+
+uniform vec3 gEmissiveColor;   // 48 255 48
+uniform float gEmissiveSpeed;  // 1 0 2
+uniform float gLogoIntensity;  // 0 0 4
 
 void intersectObjects(inout Intersection intersection, inout Ray ray) {
     float d;
@@ -225,9 +226,11 @@ void intersectObjects(inout Intersection intersection, inout Ray ray) {
             intersection.emission = vec3(0.0);
             intersection.transparent = false;
             intersection.refractiveIndex = 1.2;
-            intersection.reflectance = 0.3;
+            intersection.reflectance = 1.0;
 
-            intersection.emission = vec3(2.0) * revisionLogo(intersection.normal.xy * 0.6, beat);
+            if (gLogoIntensity > 0.0) {
+                intersection.emission = vec3(gLogoIntensity) * revisionLogo(intersection.normal.xy * 0.6, beat);
+            }
         } else {
             intersection.baseColor = vec3(gBaseColor);
             intersection.roughness = gRoughness;

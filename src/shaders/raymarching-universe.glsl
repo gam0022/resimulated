8f63f134ef268191cf48543c716ef4fcf16d524f
gam0022 <gam0022@gmail.com>
2020-04-05 16:50:25 +0900

    wip

--- src/shaders/raymarching-universe.glsl
+++ src/shaders/raymarching-universe.glsl
@@ -137,7 +137,7 @@ float dPlanetsMix(vec3 p) {
 uniform vec3 gPlanetPalA;  // 127 127 127
 uniform vec3 gPlanetPalB;  // 110 115 115
 uniform vec3 gPlanetPalC;  // 256 178 102
-uniform vec3 gPlanetPalD;  // 10 21 25
+uniform vec3 gPlanetPalD;  // 130 130 25
 
 float planetPattern(vec2 p, float seed) {
     vec3 rand = hash31(seed);

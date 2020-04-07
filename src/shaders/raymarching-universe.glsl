55aae33c012f09bd449851547e76be0e14503fb6
gam0022 <gam0022@gmail.com>
2020-04-07 21:00:24 +0900

    wip renderVolume

--- src/shaders/raymarching-universe.glsl
+++ src/shaders/raymarching-universe.glsl
@@ -1,4 +1,4 @@
-// #define STRIP_FIXED
+#define STRIP_FIXED
 
 uniform float gSceneId;   // 0 0 2 scene
 uniform float gSceneEps;  // 0.002 0.00001 0.01
@@ -670,6 +670,49 @@ void calcRadiance(inout Intersection intersection, inout Ray ray) {
     }
 }
 
+uniform float gVolumeStepSize;  // 0.1 0 1
+
+void renderVolume(inout Intersection intersection, inout Ray ray) {
+#define iterations 17
+#define formuparam 0.53
+
+#define volsteps 20
+#define stepsize 0.1
+
+#define tile 0.850
+
+#define brightness 0.0015
+#define darkmatter 0.300
+#define distfading 0.730
+#define saturation 0.850
+
+    // volumetric rendering
+    float s = 0.1, fade = 1.;
+    vec3 v = vec3(0.);
+    for (int r = 0; r < volsteps; r++) {
+        if (s > intersection.distance) break;
+
+        vec3 p = ray.origin + s * ray.direction * .5;
+        p = abs(vec3(tile) - mod(p, vec3(tile * 2.)));  // tiling fold
+        float pa, a = pa = 0.;
+        for (int i = 0; i < iterations; i++) {
+            p = abs(p) / dot(p, p) - formuparam;  // the magic formula
+            a += abs(length(p) - pa);             // absolute sum of average change
+            pa = length(p);
+        }
+        float dm = max(0., darkmatter - a * a * .001);  // dark matter
+        a *= a * a;                                     // add contrast
+        if (r > 6) fade *= 1. - dm;                     // dark matter, don't render near
+        v += vec3(dm, dm * .5, 0.);
+        v += fade;
+        v += vec3(s, s * s, s * s * s * s) * a * brightness * fade;  // coloring based on distance
+        fade *= distfading;                                          // distance fading
+        s += gVolumeStepSize;
+    }
+    v = mix(vec3(length(v)), v, saturation);  // color adjust
+    intersection.color += clamp(0.01 * v, 0.0, 0.4);
+}
+
 uniform float gShockDistortion;    // 0 0 1 distortion
 uniform float gExplodeDistortion;  // 0 0 1
 
@@ -707,6 +750,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     for (int bounce = 0; bounce < 2; bounce++) {
         calcRadiance(intersection, ray);
+        renderVolume(intersection, ray);
         color += reflection * intersection.color;
         if (!intersection.hit || intersection.reflectance == 0.0) break;
         reflection *= intersection.reflectance;

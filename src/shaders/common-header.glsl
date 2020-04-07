37a8369236f2fbfab1375ee6ede42cfffbbc9245
gam0022 <gam0022@gmail.com>
2020-04-08 00:52:07 +0900

    wip kaneta cloud

--- src/shaders/common-header.glsl
+++ src/shaders/common-header.glsl
@@ -125,6 +125,37 @@ float fbm(in vec2 uv) {
     return f;
 }
 
+// 3D fbm
+// https://www.shadertoy.com/view/WdXGRj
+float hash(float n) { return fract(sin(n) * 43758.5453); }
+
+float noise(in vec3 x) {
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+
+    f = f * f * (3.0 - 2.0 * f);
+
+    float n = p.x + p.y * 57.0 + 113.0 * p.z;
+
+    float res = mix(mix(mix(hash(n + 0.0), hash(n + 1.0), f.x), mix(hash(n + 57.0), hash(n + 58.0), f.x), f.y),
+                    mix(mix(hash(n + 113.0), hash(n + 114.0), f.x), mix(hash(n + 170.0), hash(n + 171.0), f.x), f.y), f.z);
+    return res;
+}
+
+float fbm(vec3 p) {
+    mat3 m = mat3(0.00, 0.80, 0.60, -0.80, 0.36, -0.48, -0.60, -0.48, 0.64);
+    float f;
+    f = 0.5000 * noise(p);
+    p = m * p * 2.02;
+    f += 0.2500 * noise(p);
+    p = m * p * 2.03;
+    f += 0.12500 * noise(p);
+    p = m * p * 2.01;
+    f += 0.06250 * noise(p);
+    return f;
+}
+
+// Common
 vec3 tap4(sampler2D tex, vec2 uv, vec2 texelSize) {
     vec4 d = texelSize.xyxy * vec4(-1.0, -1.0, 1.0, 1.0);
 
--- src/shaders/raymarching-universe.glsl
+++ src/shaders/raymarching-universe.glsl
@@ -672,45 +672,56 @@ void calcRadiance(inout Intersection intersection, inout Ray ray) {
 
 uniform float gVolumeStepSize;  // 0.1 0 1
 
+float densityMap(vec3 p) {
+    vec3 q = p - vec3(0.0, 0.5, 1.0) * iTime;
+    float f = fbm(q);
+    float s1 = 1.0 - length(p * vec3(0.5, 1.0, 0.5)) + f * 2.2;
+    return saturate(s1);
+}
+
 void renderVolume(inout Intersection intersection, inout Ray ray) {
-#define iterations 17
-#define formuparam 0.53
+#define MAX_STEPS 48
+#define SHADOW_STEPS 8
+#define VOLUME_LENGTH 15.
+#define SHADOW_LENGTH 2.
 
-#define volsteps 20
-#define stepsize 0.1
+    float density = 0.;
 
-#define tile 0.850
+    float stepLength = VOLUME_LENGTH / float(MAX_STEPS);
+    float shadowStepLength = SHADOW_LENGTH / float(SHADOW_STEPS);
+    vec3 light = normalize(vec3(1.0, 2.0, 1.0));
 
-#define brightness 0.0015
-#define darkmatter 0.300
-#define distfading 0.730
-#define saturation 0.850
+    vec4 sum = vec4(0., 0., 0., 1.);
 
-    // volumetric rendering
-    float s = 0.1, fade = 1.;
-    vec3 v = vec3(0.);
-    for (int r = 0; r < volsteps; r++) {
-        if (s > intersection.distance) break;
+    vec3 pos = ray.origin + ray.direction;
 
-        vec3 p = ray.origin + s * ray.direction * .5;
-        p = abs(vec3(tile) - mod(p, vec3(tile * 2.)));  // tiling fold
-        float pa, a = pa = 0.;
-        for (int i = 0; i < iterations; i++) {
-            p = abs(p) / dot(p, p) - formuparam;  // the magic formula
-            a += abs(length(p) - pa);             // absolute sum of average change
-            pa = length(p);
+    for (int i = 0; i < MAX_STEPS; i++) {
+        if (sum.a < 0.1) {
+            break;
         }
-        float dm = max(0., darkmatter - a * a * .001);  // dark matter
-        a *= a * a;                                     // add contrast
-        if (r > 6) fade *= 1. - dm;                     // dark matter, don't render near
-        v += vec3(dm, dm * .5, 0.);
-        v += fade;
-        v += vec3(s, s * s, s * s * s * s) * a * brightness * fade;  // coloring based on distance
-        fade *= distfading;                                          // distance fading
-        s += gVolumeStepSize;
+        float d = densityMap(pos);
+
+        if (d > 0.001) {
+            vec3 lpos = pos + light;
+            float shadow = 0.;
+
+            for (int s = 0; s < SHADOW_STEPS; s++) {
+                lpos += light * shadowStepLength;
+                float lsample = densityMap(lpos);
+                shadow += lsample;
+            }
+
+            density = clamp((d / float(MAX_STEPS)) * 20.0, 0.0, 1.0);
+            float s = exp((-shadow / float(SHADOW_STEPS)) * 3.);
+            sum.rgb += vec3(s * density) * vec3(1.1, 0.9, .5) * sum.a;
+            sum.a *= 1. - density;
+
+            sum.rgb += exp(-densityMap(pos + vec3(0, 0.25, 0.0)) * .2) * density * vec3(0.15, 0.45, 1.1) * sum.a;
+        }
+        pos += ray.direction * stepLength;
     }
-    v = mix(vec3(length(v)), v, saturation);  // color adjust
-    intersection.color += clamp(0.01 * v, 0.0, 0.4);
+
+    intersection.color += saturate(sum.rgb);
 }
 
 uniform float gShockDistortion;    // 0 0 1 distortion

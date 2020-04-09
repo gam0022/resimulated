d0f2ed470420c479c937a5d2aed3ac00a127d133
gam0022 <gam0022@gmail.com>
2020-04-09 23:50:09 +0900

    Rename src/shaders/{sound-template.glsl => sound-resimulated.glsl}

--- src/shaders/sound-resimulated.glsl
+++ src/shaders/sound-resimulated.glsl
@@ -1,3 +1,23 @@
+#version 300 es
+precision mediump float;
+uniform float iSampleRate;
+uniform float iBlockOffset;
+
+vec2 mainSound(float time);
+
+out vec4 outColor;
+void main() {
+    float t = iBlockOffset + ((gl_FragCoord.x - 0.5) + (gl_FragCoord.y - 0.5) * 512.0) / iSampleRate;
+    vec2 y = mainSound(t);
+    vec2 v = floor((0.5 + 0.5 * y) * 65536.0);
+    vec2 vl = mod(v, 256.0) / 255.0;
+    vec2 vh = floor(v / 256.0) / 255.0;
+    outColor = vec4(vl.x, vh.x, vl.y, vh.y);
+}
+
+//--------------------
+// ここから下を書き換える
+//--------------------
 #define BPM 140.0
 #define PI 3.141592654
 #define TAU 6.283185307

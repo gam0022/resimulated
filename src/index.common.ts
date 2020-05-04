5d054e17d606a5d94dfbd4de393b91031b60529a
gam0022 <gam0022@gmail.com>
2020-05-05 02:31:09 +0900

    Rename src/shaders/raymarching-universe-kaneta.glsl -> src/shaders/raymarching-universe-kaneta-fms-cat.glsl

--- src/index.common.ts
+++ src/index.common.ts
@@ -13,7 +13,7 @@ export const chromatiq = new Chromatiq(
     [
         require("./shaders/raymarching-mandel.glsl").default,
         require("./shaders/raymarching-universe.glsl").default,
-        require("./shaders/raymarching-universe-kaneta.glsl").default,
+        require("./shaders/raymarching-universe-kaneta-fms-cat.glsl").default,
         require("./shaders/text-resimulated.glsl").default,
         require("./shaders/post-effect.glsl").default,
         // require("./shaders/effects/debug-circle.glsl").default,
